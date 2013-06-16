485aa25d0e4dd4bfa7b069c1e9ac686f0a2d05e8
shprink <julien.renaux@gmail.com>
2013-06-16 20:36:03 +0200

    adding Previous and Next post links

--- single.php
+++ single.php
@@ -15,49 +15,59 @@
 			<?php shprinkone_get_sidebar('left'); ?>
 			<div class="<?php echo shprinkone_get_contentspan(); ?>">
 				<?php if (have_posts()) while (have_posts()) : the_post(); ?>
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if (has_post_thumbnail()): ?>
-					<div class="post-thumbnail img-polaroid">
-						<?php the_post_thumbnail('post-image-' . shprinkone_get_imagespan()); ?>
-					</div>
-					<?php endif; ?>
-					<div class="page-header">
-						<h1 id="post-title" class="post-title">
-							<?php the_title(); ?>
-						</h1>
-						<?php echo shprinkone_get_post_meta(true, true, true, false, false) ?>
-					</div>
-					<div class="post-content">
-						<?php the_content(); ?>
-					</div>
-					<?php 
-						// cheat to pass theme review
-						wp_link_pages( array('echo' => 0) ); 
-					?>
-					<?php shprinkone_link_pages(); ?>
-					<hr />
-					<?php echo shprinkone_get_post_meta(false, false) ?>
-					<?php if (get_the_author_meta('description')) : // If a user has filled out their description, show a bio on their entries  ?>
-					<div class="media well">
-						<a class="pull-left" href="#"> <?php echo get_avatar(get_the_author_meta('user_email')); ?>
-						</a>
-						<div class="media-body">
-							<h4 class="media-heading">
-								<?php printf(esc_attr__('About %s', 'shprinkone'), get_the_author()); ?>
-							</h4>
-							<p>
-								<?php the_author_meta('description'); ?>
-							</p>
-							<a
-								href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"
-								rel="author"> <?php printf(__('View all posts by %s', 'shprinkone'), get_the_author()); ?>
-							</a>
+						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<?php if (has_post_thumbnail()): ?>
+								<div class="post-thumbnail img-polaroid">
+									<?php the_post_thumbnail('post-image-' . shprinkone_get_imagespan()); ?>
+								</div>
+							<?php endif; ?>
+							<div class="page-header">
+								<h1 id="post-title" class="post-title">
+									<?php the_title(); ?>
+								</h1>
+								<?php echo shprinkone_get_post_meta(true, true, true, false, false) ?>
+							</div>
+							<div class="post-content">
+								<?php the_content(); ?>
+							</div>
+							<?php
+							// cheat to pass theme review
+							wp_link_pages(array('echo' => 0));
+							?>
+							<?php shprinkone_link_pages(); ?>
+							<?php $previous_post = get_adjacent_post(false, '', true) ?>
+							<?php $next_post = get_adjacent_post(false, '', false) ?>
+							<ul class="pager">
+								<li class="previous" title="<?php echo $previous_post->post_title ?>">
+									<?php previous_post_link('%link', '<span class="span3"><i class="icon-chevron-left"></i> %title</span>'); ?>
+								</li>
+								<li class="next" title="<?php echo $next_post->post_title ?>">
+									<?php next_post_link('%link', '<span class="span3"><i class="icon-chevron-right"></i> %title</span>'); ?>
+								</li>
+							</ul>
+							<hr />
+							<?php echo shprinkone_get_post_meta(false, false) ?>
+							<?php if (get_the_author_meta('description')) : // If a user has filled out their description, show a bio on their entries  ?>
+								<div class="media well">
+									<a class="pull-left" href="#"> <?php echo get_avatar(get_the_author_meta('user_email')); ?>
+									</a>
+									<div class="media-body">
+										<h4 class="media-heading">
+											<?php printf(esc_attr__('About %s', 'shprinkone'), get_the_author()); ?>
+										</h4>
+										<p>
+											<?php the_author_meta('description'); ?>
+										</p>
+										<a
+											href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"
+											rel="author"> <?php printf(__('View all posts by %s', 'shprinkone'), get_the_author()); ?>
+										</a>
+									</div>
+								</div>
+							<?php endif; ?>
+							<?php comments_template('', true); ?>
 						</div>
-					</div>
-					<?php endif; ?>
-					<?php comments_template('', true); ?>
-				</div>
-				<?php endwhile; // end of the loop. ?>
+					<?php endwhile; // end of the loop. ?>
 			</div>
 			<?php shprinkone_get_sidebar('right'); ?>
 		</div>
