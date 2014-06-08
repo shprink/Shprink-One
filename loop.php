fa78fa51c74ebf121ec144bf798f670324cad686
shprink <julien.renaux@gmail.com>
2014-06-09 00:04:11 +0200

    adding disqus support on loop page

--- loop.php
+++ loop.php
@@ -90,6 +90,18 @@ if (isset($options['theme_posts']['meta']) && $options['theme_posts']['meta']) {
 				placement: 'top',
 				container: 'body'
 			});
+
+            // Disqus support
+			if (typeof DISQUSWIDGETS !== 'undefined') {
+                $.each(el.find('.dsq-postid'), function(i, node){
+                    var $node= $(node),
+                    $link = $node.parent('a'),
+					url = $link.attr('href').split('#', 1);
+                    $link.attr('data-disqus-identifier', $node.attr('rel'));
+					$link.attr('href', ((url.length === 1)? url[0] : url[1]) + '#disqus_thread');
+                });
+				DISQUSWIDGETS.getCount();
+			}
 		};
 
 		onAfterLoaded($container.find('.box'));
